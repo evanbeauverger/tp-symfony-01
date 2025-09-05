4e4862a78acf03fac4188ca4a84dac42706c0671
beauverger <beauverger@sio.ndlp>
2025-09-05 14:16:23 +0200

    Add Player repository

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,13 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Player;
+use App\Repository\PlayerRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PlayerController extends AbstractController
 {
+
+    #[Route('/player/', name: 'app_player')]
+    public function index(PlayerRepository $playerRepository): Response
+    {
+        $players = $playerRepository->findAll();
+
+        return $this->render('player/index.html.twig', [
+            'players' => $players,
+        ]);
+    }
+
     #[Route('/player/create', name: 'create_player')]
     public function create(EntityManagerInterface $entityManager): Response
     {
--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -2,35 +2,35 @@
 
 namespace App\Entity;
 
+use App\Repository\PlayerRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
 #[ORM\Table(name: "tbl_players")]
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
 class Player
 {
-    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: "integer")]
-    private int $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    #[ORM\Column(type: "string", length: 100)]
-    private string $name;
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
-    public function setName(): self
-    {
-        $this->name = $name;
-        return $this;
-    }
-    public function getName(): int
+
+    public function getName(): ?string
     {
         return $this->name;
     }
 
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
