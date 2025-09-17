6f8a77ab75860b04d42df38131cf9ae2e7192b4f
beauverger <beauverger@sio.ndlp>
2025-09-17 11:59:12 +0200

    add delete player

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -32,7 +32,8 @@ class PlayerController extends AbstractController
         if ($player) {
             $this->entityManager->remove($player);
             $this->entityManager->flush();
-            return new Response('Player with id ' .$id. ' deleted');
+//          return new Response('Player with id ' . $id . ' deleted <a href="/player/">Retour</a>');
+            return $this->render('player/delete.html.twig');
         } else {
             return new Response('Player with id ' .$id. ' not found', 404);
         }
